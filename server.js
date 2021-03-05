4c9c1c053f5ea64cf8340131d0080d5d0be64a29
StaceyFolsom <staceyannfolsom@gmail.com>
2021-03-04 19:21:04 -0500

    adding heroku

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ const app = express();
 app.use(express.json());
 
 // define the port
-const port = 3000;
+// const port = 3000;
+
+// define the port
+// Use Heroku's PORT or default to 3000.
+const port = process.env.PORT || 3000;
+// run the server
+app.listen(port, () => console.log(`Listening on
+port: ${port}.`));
 
 const routes = require('./routes.js');
 app.use('/', routes);
