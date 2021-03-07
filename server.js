4ccd5593e53702b3fe9c9279b3964015bca82ba0
StaceyFolsom <staceyannfolsom@gmail.com>
2021-03-07 18:53:09 -0500

    Added Heroku database connection

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// define the port
+// Use Heroku's PORT or default to 3000.
+const port = process.env.PORT || 3000;
+
 // require the Express module
 const express = require("express");
 
@@ -8,28 +12,17 @@ const app = express();
 //paths params, and body all in the request object
 app.use(express.json());
 
-// define the port
-// const port = 3000;
-
-// define the port
-// Use Heroku's PORT or default to 3000.
-const port = process.env.PORT || 3000;
 // run the server
-app.listen(port, () => console.log(`Listening on
-port: ${port}.`));
-
+app.listen(port, () => console.log(`Listening on port: ${port}.`));
 const routes = require('./routes.js');
 app.use('/', routes);
-// run the server
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
-});
 
-app.get("/healthCheck", (req, res) => {
-    res.json("Healthcheck endpoint is working.");
+
+app.get("/test", (req, res) => {
+    res.json("Woo hoo!");
   });
   
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Welcome to our quiz");
   });
   
