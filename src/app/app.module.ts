f1f7b703d9a1023397cbe236818f4f97780725ce
StaceyFolsom <staceyannfolsom@gmail.com>
2021-03-11 08:30:03 -0500

    points are totaling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
+import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizComponent
+    QuizComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
