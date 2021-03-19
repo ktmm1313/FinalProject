a72d1c26923c4dbab704e44376f7094c8a1545e1
StaceyFolsom <staceyannfolsom@gmail.com>
2021-03-19 13:45:35 -0400

    heroku hosting

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,6 +58,10 @@
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 (window as any).global = window;
+global.Buffer = global.Buffer || require('buffer').Buffer;
+(window as any).process = {
+  version: ''
+};
 
 import * as process from 'process';
 window['process'] = process;
