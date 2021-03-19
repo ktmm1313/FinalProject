02f3a5cbbc2b1ebcdeecfd83be8009f380c9c775
StaceyFolsom <staceyannfolsom@gmail.com>
2021-03-18 23:15:44 -0400

    trying to connect to remote heroku db

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MarvelService } from '../marvel.service';
-import { environment } from 'src/environments/environment'
+import { environment } from 'src/environments/environment.prod'
 
 interface Hero {
   id: number;
