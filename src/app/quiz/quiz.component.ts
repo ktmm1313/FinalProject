4fcd8ca6551e0120ba735912c3f0641779cdf9d4
StaceyFolsom <staceyannfolsom@gmail.com>
2021-03-20 12:43:44 -0400

    fixed environment import

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 import { MarvelService } from '../marvel.service';
 
 export interface Question {
