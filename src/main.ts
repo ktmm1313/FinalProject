0bb61d5ea1a66cf97db24858534f01362d578bbd
StaceyFolsom <staceyannfolsom@gmail.com>
2021-03-19 21:22:09 -0400

    heroku

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
-// import { enableProdMode } from '@angular/core';
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-// import { AppModule } from './app/app.module';
-// import { environment } from './environments/environment';
-
-// if (environment.production) {
-//   enableProdMode();
-// }
-
-// platformBrowserDynamic().bootstrapModule(AppModule)
-//   .catch(err => console.error(err));
-
-
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+if (environment.production) {
+  enableProdMode();
 }
-bootstrap();
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
