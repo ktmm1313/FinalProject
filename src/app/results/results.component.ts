43445d736aa21003033a95799f16d1ed62c22b75
StaceyFolsom <staceyannfolsom@gmail.com>
2021-03-20 19:39:44 -0400

    fixing getNemesis

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -28,7 +28,8 @@ heroDescription: string;
   getNemesis(altid: number) {
     console.log(`getNemesis altid value: ` + altid);
     console.log(`button counter started at: ` + this.counter);
-      this.http.get("http://localhost:3000/" + altid).subscribe( response => {
+      // this.http.get("http://localhost:3000/" + altid).subscribe( response => {
+        this.http.get(environment.backendUrl + altid).subscribe( response => {
       let hero = response[0];  //used to be const
         let id = hero.marvelid;
         this.getCharacter.altid = hero.altid;
